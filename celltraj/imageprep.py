ddcfd191838b09009176033d63f864ead5f52f9a
copperma <copperma@metal-1-0.local>
2022-09-07 09:55:28 -0700

    added functions

--- celltraj/imageprep.py
+++ celltraj/imageprep.py
@@ -21,6 +21,7 @@ import pyemma.coordinates.clustering
 import re
 from pystackreg import StackReg
 from skimage import transform as tf
+from sklearn.linear_model import LinearRegression
 
 def list_images(imagespecifier):
     """list images in a directory matching a pattern..
@@ -197,13 +198,13 @@ def get_mask_2channel_ilastik(file_ilastik,fore_channel=0,holefill_area=0,pcut=0
     msk_fore=msk_fore>pcut
     return msk_fore
 
-def get_masks(masklist,fore_channel=0,holefill_area=0):
+def get_masks(masklist,fore_channel=0,holefill_area=0,pcut=0.8):
     nF=len(masklist)
     masks=[None]*nF
     for iF in range(nF):
         file_ilastik=masklist[iF]
         print('loading '+file_ilastik)
-        msk=get_mask_2channel_ilastik(file_ilastik,fore_channel=fore_channel,holefill_area=holefill_area)
+        msk=get_mask_2channel_ilastik(file_ilastik,fore_channel=fore_channel,holefill_area=holefill_area,pcut=pcut)
         masks[iF]=msk
     return masks
 
@@ -417,4 +418,83 @@ def expand_registered_images(imgs,tSet):
         imgst[iS,:,:]=img
     return imgst
 
+def save_frame_h5(filename,frame,img=None,msks=None,fmsk=None,overwrite=False,timestamp=None):
+    iS=frame
+    if timestamp is None:
+        timestamp=float(frame)
+    f=h5py.File(filename,'a')
+    if img is not None:
+        dsetName="/images/img_%d/image" % int(iS)
+        try:
+            dset = f.create_dataset(dsetName, np.shape(img))
+            dset[:] = img
+            dset.attrs['time']=timestamp
+        except:
+            sys.stdout.write('image '+str(iS)+' exists\n')
+            if overwrite:
+                del f[dsetName]
+                dset = f.create_dataset(dsetName, np.shape(img))
+                dset[:] = img
+                dset.attrs['time']=timestamp
+                sys.stdout.write('    ...overwritten\n')
+    if msks is not None:
+        dsetName="/images/img_%d/mask" % int(iS)
+        try:
+            dset = f.create_dataset(dsetName, np.shape(msks))
+            dset[:] = msks
+            dset.attrs['time']=timestamp
+        except:
+            sys.stdout.write('mask '+str(iS)+' exists\n')
+            if overwrite:
+                del f[dsetName]
+                dset = f.create_dataset(dsetName, np.shape(msks))
+                dset[:] = msks
+                dset.attrs['time']=timestamp
+                sys.stdout.write('    ...overwritten\n')
+    if fmsk is not None:
+        dsetName="/images/img_%d/fmsk" % int(iS)
+        try:
+            dset = f.create_dataset(dsetName, np.shape(fmsk))
+            dset[:] = fmsk
+            dset.attrs['time']=timestamp
+        except:
+            sys.stdout.write('fmsk '+str(iS)+' exists\n')
+            if overwrite:
+                del f[dsetName]
+                dset = f.create_dataset(dsetName, np.shape(fmsk))
+                dset[:] = fmsk
+                dset.attrs['time']=timestamp
+                sys.stdout.write('    ...overwritten\n')
+    f.close()
 
+def get_linear_coef(counts0,countsA,countsB,countsAB):
+    regress_linear = LinearRegression(fit_intercept=False)
+    regress_linear.fit(np.array([countsA,countsB]).T,countsAB)
+    return regress_linear.coef_
+
+def get_linear_batch_normalization(feat0_all,feat1_all,nbins=100,return_coef=False,stdcut=5): #fit linear normalization feat1=a*feat0+b from histogram
+    feat1=feat1_all[np.abs((feat1_all-np.nanmean(feat1_all))/np.nanstd(feat1_all))<stdcut]
+    feat0=feat0_all[np.abs((feat0_all-np.nanmean(feat0_all))/np.nanstd(feat0_all))<stdcut]
+    bins_feat=np.linspace(np.min(np.append(feat0,feat1)),np.max(np.append(feat0,feat1)),nbins+1)
+    prob0,bins0=np.histogram(feat0,bins=bins_feat)
+    prob1,bins1=np.histogram(feat1,bins=bins0)
+    cprob0=np.cumsum(prob0/np.sum(prob0)); cprob1=np.cumsum(prob1/np.sum(prob1))
+    bins=.5*bins0[0:-1]+.5*bins0[1:]
+    cbins_edges=np.linspace(np.min(np.append(cprob0,cprob1)),1,nbins+1)
+    cbins=.5*cbins_edges[0:-1]+.5*cbins_edges[1:]
+    inv_cprob1=np.zeros(nbins)
+    for ibin in range(nbins):
+        imatch=np.argmin(np.abs(cprob1-cbins[ibin]))
+        inv_cprob1[ibin]=bins[imatch]
+    inds_bins_cprob0=np.digitize(feat0,np.append(bins_feat[0:-1],np.inf))-1
+    cprob1_feat0=cprob0[inds_bins_cprob0]
+    inds_bins_inv_cprob1=np.digitize(cprob1_feat0,np.append(cbins_edges[0:-1],np.inf))-1
+    feat1_est=inv_cprob1[inds_bins_inv_cprob1]
+    regress_linear = LinearRegression(fit_intercept=True)
+    regress_linear.fit(np.array([feat1_est]).T,feat0)
+    if return_coef:
+        a=regress_linear.coef_[0]
+        b=regress_linear.intercept_
+        return a,b
+    else:
+        return regress_linear.predict(np.array([feat0_all]).T)
