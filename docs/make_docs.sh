84f3ed9d0b92b7d57602d3bb54c5c3f4e749dc63
jcopperm <jcopperm@gmail.com>
2024-04-25 12:59:01 -0700

    updated docs

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -4,6 +4,7 @@ make html
 rm -r stubs
 cp _build/html/*.html ./
 cp -r _build/html/stubs ./
-pandoc -f html -t markdown readme.html -o README.md
-cp README.md ../
+#pandoc -f html -t markdown readme.html -o README.md
+#cp README.md ../
+cp index.html ../
 
